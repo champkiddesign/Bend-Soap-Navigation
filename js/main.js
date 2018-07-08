cf44dfb0619a6541483524b84830995c5a747321
champkiddesign <greg@champkiddesign.com>
2018-07-07 18:44:17 -0700

    Desktop Fixes
    - Fix Javascript issue with hover effect
    - Added style changes to best reflect prototype
    -Added CSS arrow to navigation
    -More bug fixes

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ jQuery(document).ready(function($){
 	});
 
 	//on desktop - differentiate between a user trying to hover over a dropdown item vs trying to navigate into a submenu's contents
-	var submenuDirection = ( !$('.cd-dropdown-wrapper').hasClass('open-to-left') ) ? 'right' : 'left';
+	var submenuDirection = ( !$('.cd-dropdown-wrapper').hasClass('open-to-left') ) ? 'below' : 'left';
 	$('.cd-dropdown-content').menuAim({
         activate: function(row) {
         	$(row).children().addClass('is-active').removeClass('fade-out');
